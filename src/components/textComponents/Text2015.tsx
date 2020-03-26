1bd20b9bfd6ffe50710d124f7317546dcd35ffdd
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2020-03-26 23:13:14 +0100

    finished timeline, connected timeline with text-components, styled components

---
+++ src/components/textComponents/Text2015.tsx
@@ -0,0 +1,50 @@
+import React, { FunctionComponent } from 'react';
+
+import edinburgh from '../../images/edinburgh.png';
+
+const Text2015: FunctionComponent = () => (
+  <div
+    className="card mb-3 textComponent"
+    style={{ backgroundColor: 'rgba(77, 118, 132, 0.74)', padding: '1rem' }}
+  >
+    <div className="row no-gutters">
+      <div className="col-md-4 imageArea">
+        <img
+          src={edinburgh}
+          className="card-img"
+          alt="..."
+          // style={{ borderRadius: '80%' }}
+        />
+      </div>
+      <div className="col-md-8 textarea">
+        <div className="card-body">
+          <h3 className="card-title">Card title</h3>
+          <p className="card-text">
+            2019 Contrary to popular belief, Lorem Ipsum is not simply random
+            text. It has roots in a piece of classical Latin literature from 45
+            BC, making it over 2000 years old. Richard McClintock, a Latin
+            professor at Hampden-Sydney College in Virginia, looked up one of
+            the more obscure Latin words, consectetur, from a Lorem Ipsum
+            passage, and going through the cites of the word in classical
+            literature, discovered the undoubtable source. Lorem Ipsum comes
+            from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+            (The Extremes of Good and Evil) by Cicero, written in 45 BC. This
+            book is a treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit
+            amet..", comes from a line in section 1.10.32. The standard chunk of
+            Lorem Ipsum used since the 1500s is reproduced below for those
+            interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et
+            Malorum" by Cicero are also reproduced in their exact original form,
+            accompanied by English versions from the 1914 translation by H.
+            Rackham. Where can I get some? There are many variations of passages
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Text2015;
--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,14 +1,22 @@
 import React, { FunctionComponent, useState } from 'react';
 import ReactHorizontalTimeline from 'react-horizontal-timeline';
 import MainLayout from '../layouts/Main';
+//import Text1993 from '../components/textComponents/Text1993';
+import Text2011 from '../components/textComponents/Text2011';
+import Text2015 from '../components/textComponents/Text2015';
+import Text2018 from '../components/textComponents/Text2018';
+import Text2019 from '../components/textComponents/Text2019';
+import Text2020 from '../components/textComponents/Text2020';
+
+const COMPONENTS = [Text2011, Text2015, Text2018, Text2019, Text2020];
 
 const VALUES = [
-  '1993-01-01',
+  //'1993-01-01',
   '2011-01-01',
   '2015-01-01',
   '2018-01-01',
   '2019-01-01',
-  '2020-01-01'
+  '2020-01-01',
 ];
 
 const EducationPage: FunctionComponent = () => {
@@ -16,28 +24,47 @@ const EducationPage: FunctionComponent = () => {
   return (
     <MainLayout>
       <div className="educationContainer">
-        <div className="educationContainerContentWrapper">
-          {/* Bounding box for the Timeline */}
-          <div className="timeline">
-            <ReactHorizontalTimeline
-              getLabel={(date: string) => new Date(date).getFullYear()}
-              styles={{
-                overflow: 'visible',
-                background: 'rgb(125, 194, 194)',
-                foreground: '#7b9d6f',
-                outline: 'rgb(198, 226, 227)'
+        <div className="educationContainerContentWrapperOuter">
+          <div className="educationContainerContentWrapper">
+            <h1
+              className="educationTitle"
+              style={{
+                backgroundColor: 'rgba(133, 227, 221, 0.61)',
               }}
-              index={timelineIndex}
-              indexClick={(index: number) => {
-                setTimelineIndex(index);
-              }}
-              values={VALUES}
-            />
+            >
+              Education
+            </h1>
+            {/* Bounding box for the Timeline */}
+            <div className="timeline">
+              <ReactHorizontalTimeline
+                getLabel={(date: string) => new Date(date).getFullYear()}
+                minEventPadding={150}
+                styles={{
+                  //overflow: 'visible',
+                  background: 'rgb(102, 143, 157)',
+                  foreground: 'rgba(137, 222, 218, 0.62)',
+                  outline: 'rgb(198, 226, 227)',
+                }}
+                index={timelineIndex}
+                indexClick={(index: number) => {
+                  setTimelineIndex(index);
+                }}
+                values={VALUES}
+              />
+            </div>
           </div>
           <div className="textWrapper">
             <div className="text-center">
-              {/* any arbitrary component can go here */}
-              {timelineIndex}
+              {VALUES.map((dateString: string, index) => {
+                const year = dateString.substring(0, 4);
+                const YearTextComponent = COMPONENTS[index];
+                const isVisible = index === timelineIndex;
+                if (!isVisible) {
+                  return null;
+                }
+
+                return <YearTextComponent key={year} />;
+              })}
             </div>
           </div>
         </div>
